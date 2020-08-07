4f55342b1878bd9d3f86bda0d0ddd9ff1331eeea
velox <xhy20010806@gmail.com>
2020-08-07 23:06:58 +0800

    main added, board.h changed

---
+++ main.cc
@@ -0,0 +1,76 @@
+#include "gameController.h"
+
+bool checkDirection(string inp)
+{
+	return (inp == "no" || inp == "so" || inp == "ea" || inp == "we" 
+				|| inp == "ne" || inp == "nw" || inp == "se" || inp == "sw");
+}
+int main(int argc, char* argv[])
+{
+	string fname = (argc > 1) ? argv[1] : "default.txt";
+	auto in = make_shared<ifstream>(fname);
+
+	bool stillModeOn = 0;
+	GameController GC;
+	int level = 1;
+	while (board.getLevel() <= 5)
+	{
+		level = board.getLevel();
+		GC.setFloor(*in);	// contains display
+		if (level == 1) { GC.choosePC(); }
+		GC.setGameElement();	// contains display
+
+		string line;
+		while (getline(cin, line))
+		{
+			istringstream iss{line};
+			string inp, arg;
+			iss >> inp;
+
+			if (checkDirection(inp))
+			{
+				GC.movePC(inp);
+				//	if PC.getPCXCoordinate == getStairX
+				//	break;
+				if (!stillMode) { GC.moveEnemy(); }
+			}
+			else if (inp == "u")
+			{
+				iss >> arg;
+				if (checkDirection(arg))
+				{
+					GC.drinkPotion(string arg);
+				}
+				if (!stillMode) { GC.moveEnemy(); }
+			}
+			else if (inp == "a")
+			{
+				iss >> arg;
+				if (checkDirection(arg)) 
+				{
+					GC.attackEnemy(string arg);
+				}
+				if (!stillMode) { GC.moveEnemy(); }
+			}
+			else if (inp == "f")
+			{
+				stillMode = 1 - stillMode;
+				if (!stillMode) { GC.moveEnemy(); }
+			}
+			else if (inp == "r")
+			{
+				level = 1;
+				bool stillModeOn = 0;
+				GC.newBoard(1);
+				break;
+			}
+			else if (inp == "q")
+			{
+				GC.displayLose();
+				return 0;
+			}
+			GC.newboard(level+1);
+		}
+	}
+	GC.displayWin();
+}
