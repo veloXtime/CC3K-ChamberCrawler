f11ec411ca8a03bcc3da2dac88ac611b2ab76222
Rivers Chen <riverscbc47@gmail.com>
2020-08-14 16:27:32 -0400

    merchant damage fixed

--- shade.cc
+++ shade.cc
@@ -1,9 +1,14 @@
 #include "shade.h"
-
+#include "debug.h"
 // Constructor
 Shade::Shade(int x, int y, char c, std::string race,
              int hp, int atk, int def, int max_hp)
-             : PlayerCharacter{x, y, c, race, hp, atk, def, max_hp} {}
+             : PlayerCharacter{x, y, c, race, hp, atk, def, max_hp} 
+             {
+                 #ifdef DEBUG
+                 this->def = 1000;
+                 #endif
+             }
 
 int Shade::getScore()
 {
