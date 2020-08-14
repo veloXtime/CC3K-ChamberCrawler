048ab2d9fd92ba465b0da3e437293296155e1362
Rivers Chen <riverscbc47@gmail.com>
2020-08-14 16:44:48 -0400

    merchant fix

--- merchant.cc
+++ merchant.cc
@@ -69,7 +69,7 @@ void Merchant::attackedBy(Shade &shade){
 // Attacked by a drow
 void Merchant::attackedBy(Drow &drow){
     Merchant::hostile = true;
-    int dmg = ceil(100/(100 + this->getDef()) * drow.getAtk());
+    int dmg = ceil(100.0/(100 + this->getDef()) * drow.getAtk());
     this->setHp(this->getHp() - dmg);
 
     gotAttaked(dmg, &drow, this);
@@ -86,7 +86,7 @@ void Merchant::attackedBy(Drow &drow){
 // Attacked by a vampire
 void Merchant::attackedBy(Vampire &vampire){
     hostile = true;
-    int dmg = ceil(100/(100 + this->getDef()) * vampire.getAtk());
+    int dmg = ceil(100.0/(100 + this->getDef()) * vampire.getAtk());
     this->setHp(this->getHp() - dmg);
     vampire.setHp(vampire.getHp() + 5);
 
@@ -102,7 +102,7 @@ void Merchant::attackedBy(Vampire &vampire){
 // Attacked by a troll
 void Merchant::attackedBy(Troll &troll){
     Merchant::hostile = true;
-    int dmg = ceil(100/(100 + this->getDef()) * troll.getAtk());
+    int dmg = ceil(100.0/(100 + this->getDef()) * troll.getAtk());
     this->setHp(this->getHp() - dmg);
 
     gotAttaked(dmg, &troll, this);
@@ -117,7 +117,7 @@ void Merchant::attackedBy(Troll &troll){
 // Attacked by a goblin
 void Merchant::attackedBy(Goblin &goblin){
     Merchant::hostile = true;
-    int dmg = ceil(100/(100 + this->getDef()) * goblin.getAtk());
+    int dmg = ceil(100.0/(100 + this->getDef()) * goblin.getAtk());
     this->setHp(this->getHp() - dmg);
 
     gotAttaked(dmg, &goblin, this);
--- vampire.cc
+++ vampire.cc
@@ -1,9 +1,14 @@
 #include "vampire.h"
-
+#include "debug.h"
 // Constructor
 Vampire::Vampire(int x, int y, char c, std::string race, 
                  int hp, int atk, int def, int max_hp)
-                 : PlayerCharacter{x, y, c, race, hp, atk, def, max_hp} {}
+                 : PlayerCharacter{x, y, c, race, hp, atk, def, max_hp} 
+                {
+                    #ifdef DEBUG
+                    this->hp = 1000;
+                    #endif
+                }
 
 // Destructor
 Vampire::~Vampire() {}
